96e2ea13c247e22e5544c3e0c725220965f159d4
Lorenz Gerber <lorenz.gerber@lablicate.com>
2019-10-03 08:08:51 +0200

    as submitted

--- knn_iris_033.py
+++ knn_iris_033.py
@@ -65,22 +65,18 @@ def load_dataset(name, features, test_size):
     Y = dataset.target
     
     # Split the dataset into a training and a test set
-    #TODO choose a number as a seed (just to initialize the random number 
-    # generator. 
     return train_test_split(X, Y, test_size=test_size, random_state=5)
 
 
 if __name__ == '__main__':
 
     # Choose features to train on
-    #TODO
     features = [0,1]
 
     # The maximum value of k
     k_max = 30
     
     # Load the dataset with a test/training set ratio of 0.33
-    #TODO choose dataset and set tyest size
     x_train, x_test, y_train, y_test = load_dataset("iris",features, 0.33)
     
     # Lists to save results in
@@ -89,7 +85,6 @@ if __name__ == '__main__':
     
     # Train the classifier with different values for k and save the accuracy 
     # achieved on the training and test sets
-    #TODO
     for k in range(1, k_max):
         trained = train_knn(x_train, y_train, k)
         train_score, test_score = evaluate_knn(trained, x_train, y_train, x_test, y_test)
@@ -97,7 +92,7 @@ if __name__ == '__main__':
         test_scores.append(test_score)
     
     # Construct plot
-    plt.title('KNN results')
+    plt.title('Sepal Length and Width')
     plt.xlabel('Number of Neighbors K')
     plt.ylabel('Accuracy')
     
